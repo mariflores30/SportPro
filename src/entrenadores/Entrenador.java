1f102d8ff6bc3244b10c3471dfb40d4bdfafc7fe
maripacita <mariichavarriajerez29@gmail.com>
2026-06-25 15:48:10 -0600

    Clase Entrenador
    Se crea la clase entrenador, con sus metodos Get, Set, Constructor y toString

---
+++ src/entrenadores/Entrenador.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entrenadores;
+
+import java.time.LocalDate;
+import personas.Persona;
+
+/**
+ *
+ * @author marii
+ */
+public class Entrenador extends Persona {
+    protected String especialidad;
+    protected int tiempoExperiencia;
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public int getTiempoExperiencia() {
+        return tiempoExperiencia;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    
+    public Entrenador(String nombre, LocalDate fecha, String telefono, String id) {
+        super(nombre, fecha, telefono, id);
+        this.especialidad= especialidad;
+        this.tiempoExperiencia= tiempoExperiencia;
+    }
+    @Override
+    public String toString(){
+        return "Nombre: "+nombre+"\nFecha Nacimiento: "+fecha+"\nTelefono: "+telefono+
+                "\nIdentificacion: "+id+"\nEspecialidad: "+especialidad+"Tiempo de Experiencia"+tiempoExperiencia;
+    
+    
+}
+}
