b21555abf699b3b61e5f667ff20c0262789be0e6
iva <ivallen84@gmail.com>
2026-06-25 14:48:24 -0600

    Se crea paquete y clase persona
    posee metodo set, get y constructor

---
+++ src/personas/Persona.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personas;
+
+import java.time.LocalDate;
+
+
+/**
+ *
+ * @author PC
+ */
+public class Persona {
+    protected String nombre;
+    protected LocalDate fecha;
+    protected String telefono;
+    protected String id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(String nombre, LocalDate fecha, String telefono, String id) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.telefono = telefono;
+        this.id = id;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
